19d01d54f6b7d0520d3c8db7bae84590603c97fc
ivanri <ivan.riboldi@outlook.com>
2019-08-18 00:15:48 -0300

    add cors & var port con .env

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import express, { json } from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import productRoutes from './../components/product/productRoutes'
 //init
 const app = express();
+let port = process.env.PORT || 3001;
+
 //middleware
 app.use(morgan('dev'))
 app.use(json())
+app.use(cors())
 
 //routes
 app.use('/api/products',productRoutes)
 
-app.listen(3001, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3001!');
 });
 

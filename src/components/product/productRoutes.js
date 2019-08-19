1ed91f9c73777029e1b5e6bff5cf0404dccf24b8
ivanri <ivan.riboldi@outlook.com>
2019-08-18 22:16:47 -0300

    fixing cors

--- src/components/product/productRoutes.js
+++ src/components/product/productRoutes.js
@@ -4,7 +4,12 @@ const api = express.Router()
 const cors = require('cors')
 var productsCtrl = require('./productController')
 //middleware
-api.options('*',cors())
+api.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 api.get('/',cors(), productsCtrl.listAllProduct)
 api.post('/',cors(), productsCtrl.addProduct)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var morgan = require('morgan')
 var productRoutes = require('./components/product/productRoutes')
+var cors = require('cors')
 //init
 var app = express();
 var port = process.env.PORT || 3001;
@@ -8,9 +9,14 @@ var port = process.env.PORT || 3001;
 //middleware
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //routes products
-app.use('/api/products', productRoutes)
+app.use('/api/products',cors(), productRoutes)
 
 app.listen(port, function () {
   console.log('Example app listening on port 3001!');

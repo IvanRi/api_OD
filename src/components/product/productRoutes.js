97841c186b517f672c0ddf2aae12bdc299329a85
ivanri <ivan.riboldi@outlook.com>
2019-08-18 22:36:50 -0300

    fixing cors -.-

--- src/components/product/productRoutes.js
+++ src/components/product/productRoutes.js
@@ -4,12 +4,7 @@ const api = express.Router()
 const cors = require('cors')
 var productsCtrl = require('./productController')
 //middleware
-api.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
+api.options('*', cors())
 
 api.get('/',cors(), productsCtrl.listAllProduct)
 api.post('/',cors(), productsCtrl.addProduct)
--- src/index.js
+++ src/index.js
@@ -9,11 +9,8 @@ var port = process.env.PORT || 3001;
 //middleware
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.options('*', cors())
+
 
 //routes products
 app.use('/api/products',cors(), productRoutes)

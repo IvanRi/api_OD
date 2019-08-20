1ed29b032ad05e761cb4941b61511b44de4fd28d
Ivan <riboldiip@globalhitss.com>
2019-08-20 13:17:12 -0300

    finally fix CORS

--- src/components/product/productRoutes.js
+++ src/components/product/productRoutes.js
@@ -1,13 +1,10 @@
 'use strict'
 const express = require('express')
 const api = express.Router()
-const cors = require('cors')
 var productsCtrl = require('./productController')
-//middleware
-api.options('*', cors())
 
-api.get('/',cors(), productsCtrl.listAllProduct)
-api.post('/',cors(), productsCtrl.addProduct)
-api.delete('/',cors(), productsCtrl.deleteProduct)
+api.get('/', productsCtrl.listAllProduct)
+api.post('/', productsCtrl.addProduct)
+api.delete('/', productsCtrl.deleteProduct)
 
 module.exports = api
--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 var express = require('express')
 var morgan = require('morgan')
 var productRoutes = require('./components/product/productRoutes')
-var cors = require('cors')
 //init
 var app = express();
 var port = process.env.PORT || 3001;
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3003');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next()
+};
+
+app.use(allowCrossDomain)
+
 //middleware
 app.use(morgan('dev'))
 app.use(express.json())
-app.options('*', cors())
 
 
 //routes products
-app.use('/api/products',cors(), productRoutes)
+app.use('/api/products', productRoutes)
 
 app.listen(port, function () {
   console.log('Example app listening on port 3001!');

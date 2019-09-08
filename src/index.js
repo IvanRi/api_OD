69134dd646d1a79dba31fd61edcb0eaab5286901
ivanri <ivan.riboldi@outlook.com>
2019-09-08 17:30:28 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ var app = express();
 var port = process.env.PORT || 3001;
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://orderdispatcher.herokuapp.com');
-  // res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Origin', 'https://orderdispatcher.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With , X-Auth-Token');
   next()
@@ -31,8 +31,8 @@ app.use(cors())
 app.use('/login',cors(), login)
 app.use('/api/products',[cors(),auth], productRoutes)
 app.use('/api/users',[cors(),auth], userRoutes)
-app.use('/api/order', orderRoutes)
-app.use('/api/product_order', productOrderRoutes)
+app.use('/api/order',cors(), orderRoutes)
+app.use('/api/product_order',cors(), productOrderRoutes)
 
 app.listen(port, function () {
   console.log('Example app listening on port 3001!');

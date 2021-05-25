2bf03d5af0596d5c48ab85664c762669b4d0f446
ivan riboldi <ivan.riboldi@outlook.com>
2021-05-25 13:13:48 -0300

    update .env configuration

--- src/db/config.js
+++ src/db/config.js
@@ -1,10 +1,13 @@
 //para poder hacer el conect a una uri le hace falta poner en 
 //on por defecto el SSL
+import dotenv from 'dotenv';
+dotenv.config();
+
 const pg = require('pg');
 pg.defaults.ssl = true;
 
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('postgres://siejympukospvw:f90c9df894f86771f8d292bdc607e631d93308fcff3c165b29a053c8a65fc81a@ec2-75-101-147-226.compute-1.amazonaws.com:5432/d5tbpa4hp8qhd1')
+const sequelize = new Sequelize(process.env.CONNECTION_STRING)
 
 
 module.exports = sequelize
